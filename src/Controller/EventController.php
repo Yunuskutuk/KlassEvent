eae064309c6bfe8e2da136230e864474ba1e9e56
caroww <caroline.ww@wanadoo.fr>
2020-12-15 14:38:51 +0100

    CSS index event + navbar event

---
+++ src/Controller/EventController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+    /**
+     * @Route("/event", name="event_")
+     */
+
+class EventController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('event/index.html.twig');
+    }
+}
