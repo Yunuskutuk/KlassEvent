9bf9f47a1f08f327127326f6051bf277928e4f43
JeffNys <sxb.jeanfrancois.nys@gmail.com>
2020-12-15 12:51:32 +0100

    crud event with debug

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -12,7 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class EventType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('type', TextType::class, [
@@ -31,7 +31,7 @@ class EventType extends AbstractType
             ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Event::class,
