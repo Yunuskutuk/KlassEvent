be7dc59e92db8df83272ebf00360c07aad8a4139
JeffNys <sxb.jeanfrancois.nys@gmail.com>
2021-01-18 16:31:57 +0100

    temp save

--- src/Controller/QuoteController.php
+++ src/Controller/QuoteController.php
@@ -3,18 +3,26 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class QuoteController extends AbstractController
 {
     /**
-     * @Route("/quote", name="quote")
+     * @Route("/quote/{id}", name="quote_quote")
      */
-    public function index(): Response
+    public function quote(string $id, Request $request): Response
     {
-        return $this->render('quote/index.html.twig', [
-            'controller_name' => 'QuoteController',
-        ]);
+        // pour faire un devis, il nous faut:
+        // 1 la date (et l'heure) de l'événement
+        // 2 nombre d'invités
+        // 3 le type d'évenement
+        // 4 les services
+        // 5 pour chaques service, les options
+        // 6 le contact
+        dd($id);
+
+        return $this->render('quote/index.html.twig');
     }
 }
