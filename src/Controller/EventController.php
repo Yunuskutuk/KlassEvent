73221e32f6f50494b534cbeedccf0528824581fa
David-Damato <71222704+David-Damato@users.noreply.github.com>
2021-01-22 10:14:15 +0100

    Update EventController.php

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -8,6 +8,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Contact;
+use App\Form\ContactType;
 
 class EventController extends AbstractController
 {
