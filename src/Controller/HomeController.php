36b03c9f76043d06429f91384b218df3203d3e4c
Yunuskutuk <yns.ktk@hotmail.com>
2021-01-25 11:30:01 +0100

    footer insta

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,23 +2,15 @@
 
 namespace App\Controller;
 
-use App\Repository\EventRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
-use App\Entity\Contact;
-use App\Form\ContactType;
-use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
