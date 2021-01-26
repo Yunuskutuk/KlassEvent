8cdeb32cf8b77a133330fe5fcc4111fd21365366
JeffNys <sxb.jeanfrancois.nys@gmail.com>
2021-01-26 18:39:30 +0100

    now we can fill DB from translate.xx.yaml with fixtures

--- migrations/Version20210126151710.php
+++ migrations/Version20210126151710.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210121122745 extends AbstractMigration
+final class Version20210126151710 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -29,6 +29,7 @@ final class Version20210121122745 extends AbstractMigration
         $this->addSql('CREATE TABLE service (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE service_option (service_id INT NOT NULL, option_id INT NOT NULL, INDEX IDX_64410586ED5CA9E6 (service_id), INDEX IDX_64410586A7C41D6F (option_id), PRIMARY KEY(service_id, option_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE service_picture (service_id INT NOT NULL, picture_id INT NOT NULL, INDEX IDX_D95F1D15ED5CA9E6 (service_id), INDEX IDX_D95F1D15EE45BDBF (picture_id), PRIMARY KEY(service_id, picture_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE translate (id INT AUTO_INCREMENT NOT NULL, yaml_key VARCHAR(255) NOT NULL, french LONGTEXT NOT NULL, turkish LONGTEXT NOT NULL, UNIQUE INDEX UNIQ_4A106377F5EBF904 (yaml_key), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, phone VARCHAR(255) NOT NULL, adress LONGTEXT DEFAULT NULL, adress_event LONGTEXT DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE week (id INT AUTO_INCREMENT NOT NULL, number INT NOT NULL, start DATE NOT NULL, end DATE NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE week_menu (week_id INT NOT NULL, menu_id INT NOT NULL, INDEX IDX_B77F3523C86F3B2F (week_id), INDEX IDX_B77F3523CCD7E912 (menu_id), PRIMARY KEY(week_id, menu_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -62,6 +63,7 @@ final class Version20210121122745 extends AbstractMigration
         $this->addSql('DROP TABLE service');
         $this->addSql('DROP TABLE service_option');
         $this->addSql('DROP TABLE service_picture');
+        $this->addSql('DROP TABLE translate');
         $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE week');
         $this->addSql('DROP TABLE week_menu');
