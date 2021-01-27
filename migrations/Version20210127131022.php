5b0f221ea79a5e935d87a34e56e49dee1e96687e
Yunuskutuk <yns.ktk@hotmail.com>
2021-01-27 16:04:29 +0100

    traduction user

--- migrations/Version20210127131022.php
+++ migrations/Version20210127131022.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210126151710 extends AbstractMigration
+final class Version20210127131022 extends AbstractMigration
 {
     public function getDescription() : string
     {
